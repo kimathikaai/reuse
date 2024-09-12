3c7e86480bb94439c5b2203e611cd5f0e5ceb5c4
kimathikaai <kiprotich.kaai@gmail.com>
2024-09-12 11:25:42 -0400

    Check resnet50 compatability

--- explore_weights.py
+++ explore_weights.py
@@ -1,19 +1,33 @@
-import torch
-import sys
 import pdb
+import sys
 
-def check_weights(weight_path:str):
+import torch
+import torchvision
+
+
+def check_weights(weight_path: str):
     """
     Use this method to begin exploring saved checkpoints
     """
-    checkpoint = torch.load(weight_path)
+
+    try:
+        checkpoint = torch.load(weight_path)
+    except:
+        checkpoint = torch.load(weight_path, map_location=torch.device("cpu"))
+
     checkpoint_type = type(checkpoint)
     print(f"{checkpoint_type = }")
     if isinstance(checkpoint, dict):
         print(f"{checkpoint.keys()}")
+
+    # Check compatabilityw with ResNet50
+    resnet50 = torchvision.models.resnet50()
+    print(f"{resnet50.state_dict().keys() = }")
+    resnet50.load_state_dict(checkpoint, strict=True)
+
     pdb.set_trace()
+
+
 
 if __name__ == "__main__":
     check_weights(*sys.argv[1:])
-
-
